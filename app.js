442c9f68ea9bf044e81d1ac9c3ee8843ec363fce
kiiiiiiiiif <kyle.vuanh@gmail.com>
2024-06-26 22:32:14 +0700

    ok

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var port = 3000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Deploying with render.com!`);
 });
