75afb0875ea16f9ba178fce87c3e415721632124
kiiiiiiiiif <kyle.vuanh@gmail.com>
2024-06-26 22:23:23 +0700

    ok

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = express();
-var port = 3001;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
-});
-
-app.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`);
 });
